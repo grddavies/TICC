b60b1e757c86bebe7c80c405a9f874310a817cf7
gdavies <gethin.davies@vestemi.com>
2020-06-16 16:00:16 +0100

    moved out of tests/

--- ticclib/tests/test_common.py
+++
@@ -1,9 +0,0 @@
-from sklearn.utils.estimator_checks import check_estimator
-from ticclib.ticc import TICC
-
-
-def test_ticc():
-    # NOTE: window size = 1 ensures input unchanged by stacking
-    # Max iter and beta of 0 to speed up test completion
-    ticc = TICC(n_clusters=3, window_size=1, max_iter=10, beta=0)
-    return check_estimator(ticc)
